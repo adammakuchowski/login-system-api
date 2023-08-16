464ce1b701c44d6586f01259ce126695b273336f
Adam Makuchowski <maku971@gmail.com>
2023-08-16 06:53:42 +0200

    update winston config

--- src/winstonConfig.ts
+++ src/winstonConfig.ts
@@ -4,12 +4,10 @@ const loggerConfig = {
   level: 'info',
   format: winston.format.combine(
     winston.format.colorize(),
-    winston.format.json(),
     winston.format.simple()
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({filename: 'combined.log'})
   ]
 }
 
