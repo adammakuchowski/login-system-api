8fe08589ec7e462f910aa5af760063cdf3ddf140
Adam Makuchowski <maku971@gmail.com>
2023-08-28 06:44:08 +0200

    update app initiation process

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,8 @@
-import createApp, {logger} from './app'
+import app, {logger} from './app'
 import appConfig from './configs/appConfig'
-import {connectDB} from './db/db'
 
-const startApp = async () => {
-  try {
-    const {port} = appConfig 
-    
-    await connectDB()
-    const express = createApp()
+const {port} = appConfig
 
-    express.listen(port, () => {
-      logger.info(`API is listening on port: ${port}`)
-    })
-  } catch (error) {
-    logger.error(error)
-
-    process.exitCode = 1
-  }
-}
-
-startApp()
+app.listen(port, () => {
+  logger.info(`API is listening on port: ${port}`)
+})
